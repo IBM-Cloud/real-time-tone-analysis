58654deda73fcef361ac0755f34be1012f4ad074
Jake Peyser <jakepeyser@gmail.com>
2016-02-11 10:44:23 -0600

    Fleshed out initial code base

--- app.js
+++ app.js
@@ -1,29 +1,82 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var express = require('express');
+var express   = require('express'),
+  app         = express(),
+  bodyParser  = require('body-parser'),
+  cfenv       = require('cfenv'),
+  extend      = require('util')._extend,
+  watson      = require('watson-developer-cloud');
 
+// Set up environment variables
 // cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+var vcapLocal = null
+try {
+  vcapLocal = require("./vcap-local.json")
+}
+catch (e) {}
 
-// create a new express server
-var app = express();
+var appEnvOpts = vcapLocal ? {vcap:vcapLocal} : {}
+var appEnv = cfenv.getAppEnv(appEnvOpts);
 
+// Configure Express
 // serve the files out of ./public as our main files
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use(bodyParser.json({ limit: '1mb' }));
 app.use(express.static(__dirname + '/public'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+// Deployment tracker code snippet
+require("cf-deployment-tracker-client").track();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+// error-handler settings
+require('./config/error-handler')(app);
 
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+// Start listening for connections
+app.listen(appEnv.port, function() {
+  console.log("server started at", appEnv.url);
 });
+
+// Configure Watson Speech to Text service
+var speechCreds = getServiceCreds(appEnv, 'rtt-speech-to-text');
+speechCreds.version = 'v1';
+var authService = watson.authorization(speechCreds);
+
+// Root page handler
+app.get('/', function(req, res) {
+  res.render('index', { ct: req._csrfToken });
+});
+
+// Get token using your credentials
+app.post('/api/token', function(req, res, next) {
+  authService.getToken({url: config.url}, function(err, token) {
+    if (err)
+      next(err);
+    else
+      res.send(token);
+  });
+});
+
+// Retrieves service credentials for the input service
+function getServiceCreds(appEnv, serviceName) {
+  var serviceCreds = appEnv.getServiceCreds(serviceName)
+  if (!serviceCreds) {
+    console.log("service " + serviceName + " not bound to this application");
+    return null;
+  }
+  return serviceCreds;
+}
